b80f383423a1be9493ff425e4229d54ef351d771
Max <maxime.klimecky@outlook.com>
2020-05-14 21:51:22 +0200

    Adapting code back to work with v3

--- firmware/components/direct_ethernet/direct_ethernet.h
+++ firmware/components/direct_ethernet/direct_ethernet.h
@@ -4,19 +4,32 @@
 #include <stdint.h>
 #include <string.h>
 
-#include <stdio.h>
-#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_eth.h"
+#include "freertos/event_groups.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_eth.h"
+#include "tcpip_adapter.h"
 
-#include "sdkconfig.h"
+#include "eth_phy/phy_lan8720.h"
 
+#define CONFIG_PHY_CLOCK_MODE 3
+#define ETHERNET_PHY_CONFIG phy_lan8720_default_ethernet_config
+#define PIN_SMI_MDC 23
+#define PIN_SMI_MDIO 18
+
+#define DEVICE_IP "192.168.1.0"
+#define DEVICE_NETMASK "255.255.255.0"
+#define DEVICE_GW "192.168.1.1"
+
+#define WIFI_CONNECTED_BIT BIT0
 #define ETHERTYPE 0xb588
 
-typedef struct {
+EventGroupHandle_t udp_event_group;
+
+typedef struct
+{
   uint8_t dst_mac[6];
   uint8_t src_mac[6];
   uint16_t ethertype;
@@ -30,13 +43,12 @@ extern uint8_t eth_src_mac[6];
 extern uint8_t eth_dst_mac[6];
 
 void (*eth_recv_cb)(uint8_t src_mac[6], uint8_t *data, int len);
-
 void (*eth_link_state_cb)(bool link_state);
 
 void eth_init();
 esp_err_t eth_send_frame(eth_frame *p_frame);
 void eth_init_frame(eth_frame *p_frame);
-void eth_send_data(uint8_t* data, int len);
+void eth_send_data(uint8_t *data, int len);
 void eth_attach_recv_cb(void (*cb)(uint8_t src_mac[6], uint8_t *data, int len));
 void eth_detach_recv_cb();
 void eth_attach_link_state_cb(void (*eth_link_state_cb)(bool link_state));
